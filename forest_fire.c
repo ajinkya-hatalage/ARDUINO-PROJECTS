d4a8e7b08592ce27378d1258145e88e6581ea2f3
TEJAS <tejaschavan296@gmail.com>
2021-10-05 17:47:16 +0530

    Forest Fire Detection

--- forest_fire.c
+++ forest_fire.c
@@ -39,3 +39,67 @@ void loop()
     ShowSerialData();
     flag = 0;
   }
+else
+  {
+    digitalWrite(9, LOW);
+    if (flag == 0)
+    {
+      flag = 1;
+      gprsSerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection
+      delay(6000);
+      ShowSerialData();
+      gprsSerial.println("AT+CIPSEND");//begin send data to remote server
+      delay(4000);
+      ShowSerialData();
+      String str = "GET https://api.thingspeak.com/update?api_key=ER43PWT91CGHQF0I&field1=" + String(0);
+      Serial.println(str);
+      gprsSerial.println(str);//begin send data to remote server
+      delay(4000);
+      ShowSerialData();
+      digitalWrite(9, LOW);
+      gprsSerial.println((char)26);//sending
+      delay(5000);//waitting for reply, important! the time is base on the condition of internet
+      gprsSerial.println();
+      ShowSerialData();
+      gprsSerial.println("AT+CIPSHUT");//close the connection
+      delay(100);
+      ShowSerialData();
+    }
+  }
+}
+void ShowSerialData()
+{
+  while (gprsSerial.available() != 0)
+    Serial.write(gprsSerial.read());
+  delay(5000);
+}
+void Module_Init()
+{
+  gprsSerial.println("AT");
+  delay(1000);
+  gprsSerial.println("AT+CPIN?");
+  delay(1000);
+  gprsSerial.println("AT+CREG?");
+  delay(1000);
+  gprsSerial.println("AT+CGATT?");
+  delay(1000);
+  gprsSerial.println("AT+CIPSHUT");
+  delay(1000);
+  gprsSerial.println("AT+CIPSTATUS");
+  delay(2000);
+  gprsSerial.println("AT+CIPMUX=0");
+  delay(2000);
+  ShowSerialData();
+  gprsSerial.println("AT+CSTT=\"www\"");
+  delay(1000);
+  ShowSerialData();
+  gprsSerial.println("AT+CIICR");
+  delay(3000);
+  ShowSerialData();
+  gprsSerial.println("AT+CIFSR");
+  delay(2000);
+  ShowSerialData();
+  gprsSerial.println("AT+CIPSPRT=0");
+  delay(3000);
+  ShowSerialData();
+}
